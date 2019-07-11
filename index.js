96bb2a21dd15c7af9c51f3062369d9f23984c42a
mee <51446936+lizandrau@users.noreply.github.com>
2019-07-11 20:01:29 +0300

    inition

---
+++ index.js
@@ -0,0 +1,52 @@
+// const Discorde = require('discord.js'),
+//       bot = new Discorde.Client(),
+//       config = require('./config.json');
+
+
+// bot.on('message', async message => {
+//   let prefix = config.prefix;
+
+//   if(message.content.startsWith(prefix + 'Hello')) {
+//     message.channel.send('Hello!');
+//   }
+// });
+
+// bot.login(config.token);
+// bot.on('ready', () => {
+//   console.log(`${bot.user.username} online`);
+//   bot.user.setPresence({status: 'dnd', game: {name: 'test', type: 0}});
+// })
+// const Discord = require('discord.js');
+// const bot = new Discord.Client();
+// const config = require('./config.json');
+
+// bot.on('massage', async message => {
+//     let prefix = config.prefix;
+
+//     if (message.content.startsWith(prefix)) {
+//         message.channel.send('Hello!');
+//     }
+// });
+
+// bot.login(config.token);
+// bot.on('ready', () => {
+//     console.log(`${bot.user.username} online`);
+//     bot.user.setPresence ({status: 'dmd', game:{name: 'coderoff', type: 0}});
+// });
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+
+bot.on('message', async message => {
+    let prefix = config.prefix;
+
+    if (message.content.startsWith(prefix)) {
+        message.channel.send('Hello!')
+    }
+});
+
+bot.login(config.token);
+bot.on('ready', () => {
+    console.log(`${bot.user.username} online`);
+    bot.user.setPresence ({status: 'dmd', game:{name: 'coder', type: 0}});
+});
